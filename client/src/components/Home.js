441975b241be3467ff1196e29496aa93ba6c318a
Maggie Zeng <tzeng@oberlin.edu>
2021-12-08 20:17:47 -0500

    added images

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import Header from '../components/Header'
+import poster from '../components/images/MainPagePoster.png'
+import "./home.css";
 
 
 function Home() {
     return (
             <div>
                 <Header/>
+                <div className="suggestions">
+                    <img src={poster} width="800" height="700" />
+                    <button className="buttons">
+                        Mask
+                    </button>
+                    <button className="buttons">
+                        Moisturizer
+                    </button>
+                    <button className="buttons">
+                        Face Wash
+                    </button>
+                    <button className="buttons">
+                        Essence
+                    </button>
+                </div>
             </div>
     );
 }
+
 
 export default Home;
