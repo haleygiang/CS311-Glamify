6bd55caef6e37d516d9c880d37acad7434d7b5bd
Maggie Zeng <tzeng@oberlin.edu>
2021-12-08 21:21:45 -0500

    allproducts pages

---
+++ client/src/components/AllProducts.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+import "./allProducts.css";
+
+function List() {
+
+    return (
+        <div>
+          <Header/>
+        </div>
+      )
+
+}
+
+export default List;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,31 +1,35 @@
-import React from 'react'
-import Header from '../components/Header'
-import poster from '../components/images/MainPagePoster.png'
+import React from "react";
+import Header from "../components/Header";
+import poster from "../components/images/MainPagePoster.png";
 import "./home.css";
-
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
-    return (
-            <div>
-                <Header/>
-                <div className="suggestions">
-                    <img src={poster} width="800" height="700" />
-                    <button className="buttons">
-                        Mask
-                    </button>
-                    <button className="buttons">
-                        Moisturizer
-                    </button>
-                    <button className="buttons">
-                        Face Wash
-                    </button>
-                    <button className="buttons">
-                        Essence
-                    </button>
-                </div>
-            </div>
-    );
-}
 
+    let navigate = useNavigate();
+    navigate('../components/AllProducts');
+
+
+  return (
+    <div>
+      <Header/>
+      <div className="suggestions">
+        <img src={poster} width="800" height="700" />
+          <buttonGroup  className="me-2" aria-label="First group">
+            <button onClick={navigate} className="b" variant="secondary">Mask</button>{" "}
+            <button onClick={navigate} className="b" variant="secondary">Moisturizer</button>{" "}
+            <button onClick={navigate} className="b" variant="secondary">Face Wash</button>{" "}
+            <button onClick={navigate} className="b" variant="secondary">Essence</button>
+          </buttonGroup>
+          <buttonGroup className="me-2" aria-label="First group">
+            <button onClick={navigate} className="b" variant="secondary">Foundation</button>{" "}
+            <button onClick={navigate} className="b" variant="secondary">Contour</button>{" "}
+            <button onClick={navigate} className="b" variant="secondary">Eyeshadow</button>{" "}
+            <button onClick={navigate} className="b" variant="secondary">Lipstick</button>
+          </buttonGroup>
+      </div>
+    </div>
+  )
+}
 
 export default Home;
