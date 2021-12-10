9f9cea2b220eb332c0fbc482552c176ed343f409
Huyen (Haley) Giang <hgiang@oberlin.edu>
2021-12-10 00:09:32 -0500

    separate container and component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,42 +1,28 @@
-import { React, useState } from "react";
+import { React } from "react";
 import "./header.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 
-function Header() {
-  const [search, setSearch] = useState("");
-  const setQuery = useState("");
-
-  // only setQuery when finish typing, setSearch back to empty string for the next search
-  const getSearch = (e) => {
-    e.preventDefault();
-    setQuery(search);
-    setSearch("");
-  };
-
-  const updateSearch = (e) => {
-    setSearch(e.target.value);
-  };
-
+function Header(props) {
   return (
     <div className="header">
       <div className="glamify">
         Glamify
         {/* search form */}
-        <form className="search-form" onSubmit={getSearch}>
+        <form className="search-form" onSubmit={props.getSearch}>
           <input
             type="text"
             className="search-bar"
-            value={search}
-            onChange={updateSearch}
+            value={props.search}
+            onChange={props.updateSearch}
             placeholder="Product's Name"
           />
           <button type="submit" className="search-button">
             Search
           </button>
         </form>
-        <button className="compare_button">compare</button>
+        <button className="compare_button">Compare</button>
       </div>
     </div>
   );
---
+++ client/src/containers/Header.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const Header = () => {
+  const [search, setSearch] = useState("");
+  const setQuery = useState("");
+
+  // only setQuery when finish typing, setSearch back to empty string for the next search
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  return { search }, getSearch, updateSearch;
+};
+
+export default Header;
