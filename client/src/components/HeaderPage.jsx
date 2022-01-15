07354dfa3304664915ddfb2b43c9d3354d80c778
Huyen (Haley) Giang <hgiang@oberlin.edu>
2022-01-15 16:02:21 -0500

    delete container; error with product detail API

--- client/src/components/HeaderPage.jsx
+++ client/src/components/HeaderPage.jsx
@@ -2,6 +2,34 @@ import { React } from "react";
 
 function Header(props) {
   const search = props.search;
+  
+  // import { useState } from "react";
+
+// const Header = () => {
+//   const [search, setSearch] = useState("");
+//   const setQuery = useState("");
+
+//   // only setQuery when finish typing, setSearch back to empty string for the next search
+//   const getSearch = (e) => {
+//     e.preventDefault();
+//     setQuery(search);
+//     setSearch("");
+//   };
+
+//   const updateSearch = (e) => {
+//     setSearch(e.target.value);
+//   };
+
+//   return { 
+//     // BUG!!!
+//       // search={search},
+//       getSearch,
+//       updateSearch
+//     }
+// };
+
+// export default Header;
+
 
   return (
     // Nav bar
