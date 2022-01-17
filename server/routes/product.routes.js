b5404a31a69aafac13b7cdeec06e56f1ca8636db
Huyen (Haley) Giang <hgiang@oberlin.edu>
2022-01-17 17:03:06 -0500

    search not done

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,11 +1,11 @@
 module.exports = app => {
     const products = require("../controllers/product.controller.js");
-  
+
     var router = require("express").Router();
-  
+
     // Create a new Product
     router.post("/", products.create);
-  
+
     // Retrieve all Products
     router.get("/", products.findAll);
 
@@ -15,21 +15,21 @@ module.exports = app => {
     // Retrieve all Products in a big category
     // Category mush be one of (makeup, skincare, hair, fragrance, bath&body)
     router.get("/bigcategory/:category", products.findByBigCategory);
-  
+
     // Retrieve all Products by search 
     router.get("/search/:keyword", products.search);
 
     // Retrieve a single Product with id
     router.get("/id/:id", products.findOne);
-  
+
     // Update a Product with id
     router.put("/:id", products.update);
-  
+
     // Delete a Product with id
     router.delete("/:id", products.delete);
-  
+
     // Delete all Products
     router.delete("/", products.deleteAll);
-    
+
     app.use('/products', router);
-  };
+};
