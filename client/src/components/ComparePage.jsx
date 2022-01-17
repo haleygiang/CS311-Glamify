56b163e7d46d278296a4afd132b6b02b75f98a31
Huyen (Haley) Giang <hgiang@oberlin.edu>
2022-01-17 15:53:58 -0500

    compare page

--- client/src/components/ComparePage.jsx
+++ client/src/components/ComparePage.jsx
@@ -1,28 +1,41 @@
-import React from 'react';
+import React from "react";
 import Header from "./HeaderPage";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  removeProductCompare,
-} from "../redux/actions/productActions";
+import { removeProductCompare } from "../redux/actions/productActions";
 
 const ComparePage = () => {
-  const products = useSelector((state) => state.compare);
+  const { products } = useSelector((state) => state.compare);
   console.log("COMPARE: ", products);
   const dispatch = useDispatch();
 
-  // Remove product from compare page 
+  // Remove product from compare page
   // eslint-disable-next-line
   const onRemove = (product) => {
     dispatch(removeProductCompare(product));
-  }
+  };
 
-    return (
-        <div>
-          {/* Nav Bar  */}
-          <Header />
-          Compare Page
+  return (
+    <div>
+      {/* Nav Bar  */}
+      <Header />
+      <div className="container mt-3 mb-3">
+        <h1 className="display-6 mb-3">Compare with Similar Products</h1>
+        <div className="table-responsive">
+          <table className="table table-striped text-successtable-border border-light">
+            <thead className="border-light">
+              <tr>
+                <th scope="col"></th>
+                {products.map(({ category }) => (
+                  <th scope="col">{category}</th>
+                ))}
+              </tr>
+              {/* {brand,category,name, love, num_reviews, price, rating, size, url} */}
+            </thead>
+          </table>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default ComparePage;
