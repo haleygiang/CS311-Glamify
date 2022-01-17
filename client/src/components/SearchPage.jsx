d34da1058a7f4da3943ce242c5287807b2b89eb3
Maggie Zeng <tzeng@oberlin.edu>
2022-01-17 15:56:58 -0500

    search page

---
+++ client/src/components/SearchPage.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../redux/actions/productActions";
+import ProductComponent from "./ProductComponent";
+
+const SearchPage = () => {
+  const lastSegment = window.location.href.split("/").pop();
+  const choice = lastSegment.substr(0,lastSegment.length);
+  const products = useSelector((state) => state.allProducts.products);
+  const dispatch = useDispatch();
+
+  // API call to fetch products from database
+  const fetchProducts = async () => {
+    const response = await axios
+      .get('http://localhost:3001/products/bigcategory/' + choice)
+      .catch((err) => {
+        console.log("Err: ", err);
+      });
+    dispatch(setProducts(response.data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  },[]); // eslint-disable-line react-hooks/exhaustive-deps
+
+
+  return <ProductComponent products={products}/>;
+};
+
+export default SearchPage;
